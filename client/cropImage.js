7f87c051bfbb8ebc6d94772b745201f2be3b4aab
wally <tecnicomdp.wz@gmail.com>
2015-05-05 19:21:57 -0300

    add interface for will be able to change plugin crop

--- client/cropImage.js
+++ client/cropImage.js
@@ -1,9 +1,47 @@
- var getResult = function(e) {
- 	 /* Return Base 64 */
-     var b64 = this.reader.result;
-     this.template.$('.image-cropper').cropit('imageSrc', b64);
+ /**
+   * Interface for implement plugin crop want to use
+   * This example use Crop it
+   * @URL https://github.com/scottcheng/cropit
+   */
+ var plugin = {
+
+ 	 /**
+ 	   * Show image in Base 64 inside crop zone html
+ 	   * @param {Object} nodeHTML - node html 
+ 	   * @Paran {object} b64 - image in base 64
+ 	   */
+	 'showImageInCrop' : function(nodeHtml,b64){
+	 	$(nodeHtml).cropit('imageSrc', b64);
+	 },
+
+	  /**
+ 	   * Export image croped to base 64 ready for to save
+ 	   * @param {object} nodeHTML - node html
+ 	   * @return {string} b64 - image base 64
+ 	   */
+	 'exportToB64' :  function(nodeHtml){
+	 	return $(nodeHtml).cropit('export', {
+					type: 'image/png',
+					quality: .9,
+					originalSize: true
+			    });
+	 }
  }
 
+
+/**
+  * Return result for FileReader and call showImageIncrop
+  */
+ var getResult = function() {
+ 	 /* Return Base 64 */
+     var b64 = this.reader.result;
+     plugin.showImageInCrop(this.template.$('.image-cropper'),b64)
+ }
+
+ /**
+  * Read files when change data in input type file
+  * and put original data inside the input hidden
+  */
  var handleFileSelect = function(evt) {
  	 var template = this.template;
      var files = evt.target.files;
@@ -20,16 +58,17 @@
      }
  };
 
+ 
+ /**
+  * Save image in Database
+  * @param {Object} template - is the current template
+  */
  var saveCropImage= function(template){
  	$(template.$('.image-cropper')).each(function(){
 
 		var 
 			originFile= template.$('.origin-file-hidden').data("cfsaf_files"),
-		    b64 = $(this).cropit('export', {
-						type: 'image/png',
-						quality: .9,
-						originalSize: true
-				   });
+		    b64 = plugin.exportToB64(this);
 
 		if (b64){
 			
